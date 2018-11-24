db3ad775d51d5b0bb32fac44ee1d509496485447
Ajar Vashisth <ajarv@hotmail.com>
2018-11-24 07:46:47 -0800

    vuejs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'))
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.json({time:new Date(),message:"What is the purpose of your visit?"})
