58099859d1b21d0bee3ca3c1e729d5b7a17dd08a
ajarv <ajarv@hotmail.com>
2018-12-04 07:31:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express()
 app.use(express.static('public'))
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/hi', function (req, res) {
     res.json({time:new Date(),message:"What is the purpose of your visit?"})
 })
 
